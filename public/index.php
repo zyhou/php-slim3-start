411757133d62936f240d51f90db2928d711085de
Maxime <richard.maxime9@gmail.com>
2016-11-03 21:46:30 +0100

    Middleware exemple simple

--- public/index.php
+++ public/index.php
@@ -2,7 +2,18 @@
 
 require '../vendor/autoload.php';
 
+class DemoMiddleware {
+
+    public function __invoke(\Slim\Http\Request $request, \Slim\Http\Response $response, $next) {
+        $response->write('<h1>Bienvenue</h1>');
+        $response = $next($request, $response);
+        $response->write('<h1>Au revoir</h1>');
+        return $response;
+    }
+}
+
 $app = new \Slim\App();
+$app->add(new DemoMiddleware());
 
 $app->get('/', function(\Slim\Http\Request $request, \Slim\Http\Response $response) {
     return $response->getBody()->write("Bonjour les gens.");
