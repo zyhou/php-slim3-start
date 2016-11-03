878c599aec18040843952e0f54014478fedc91f9
Maxime <richard.maxime9@gmail.com>
2016-11-03 22:34:42 +0100

    Controller dans un autre fichier, simplification route

---
+++ app/Controllers/PagesController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace  App\Controllers;
+
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\ResponseInterface;
+
+class PagesController {
+
+    private $container;
+
+    public function __construct($container) {
+        $this->container = $container;
+    }
+
+    public  function home(RequestInterface $request, ResponseInterface $response){
+       $response->getBody()->write('Bonjour !');
+    }
+}
--- public/index.php
+++ public/index.php
@@ -2,65 +2,12 @@
 
 require '../vendor/autoload.php';
 
-class DemoMiddleware {
+$app = new \Slim\App([
+    'setting' => [
+        'displayErrorDetails' => true
+    ]
+]);
 
-    public function __invoke(\Slim\Http\Request $request, \Slim\Http\Response $response, $next) {
-        $response->write('<h1>Bienvenue</h1>');
-        $response = $next($request, $response);
-        $response->write('<h1>Au revoir</h1>');
-        return $response;
-    }
-}
-
-class DataBaseContainer {
-
-    /**
-     * @var PDO
-     */
-    private $pdo;
-
-    public  function __construct(PDO $pdo){
-        $this->pdo = $pdo;
-    }
-
-    public function query($sql) {
-        $req = $this->pdo->prepare($sql);
-        $req->execute();
-        return $req->fetchAll();
-    }
-}
-
-class PagesController {
-
-    private $container;
-
-    public function __construct($container)
-    {
-        $this->container = $container;
-    }
-
-    public  function salut(\Slim\Http\Request $request, \Slim\Http\Response $response, $args){
-        $posts = $this->container->db->query("select * from posts");
-        var_dump($posts);
-        return $response->write("Bonjour ".$args['nom']);
-    }
-}
-
-$app = new \Slim\App();
-
-$container = $app->getContainer();
-$container['pdo'] = function() {
-    $pdo = new PDO('mysql:dbname=slim3;host=localhost', 'root');
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    return $pdo;
-};
-
-$container['db'] = function($container) {
-    return new DataBaseContainer($container->pdo);
-};
-
-$app->add(new DemoMiddleware());
-
-$app->get('/salut/{nom}', 'PagesController:salut');
+$app->get('/', \App\Controllers\PagesController::class . ':home');
 
 $app->run();
